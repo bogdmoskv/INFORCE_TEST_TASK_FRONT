e53f44a8ecdbf7b3bd896092430d95a13ac824e4
Bogdan <bogd.moskv@gmail.com>
2023-08-23 22:27:21 +0300

    Add: navbar with menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { AuthService } from './services/auth/auth.service';
+
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'inforce-front-BM-project';
+
+  constructor(private authService:AuthService){
+
+  }
+
+  isUserLoggedIn(): boolean {
+    return this.authService.isLoggedIn();
+  }
 }
