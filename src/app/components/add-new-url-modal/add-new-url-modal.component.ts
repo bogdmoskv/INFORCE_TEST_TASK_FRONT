2ed32e053b8d45c400ce61aeedbc77ef913664c4
Bogdan <bogd.moskv@gmail.com>
2023-08-24 11:15:34 +0300

    Start sending on backend info from modal info

--- src/app/components/add-new-url-modal/add-new-url-modal.component.ts
+++ src/app/components/add-new-url-modal/add-new-url-modal.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { MdbModalRef } from 'mdb-angular-ui-kit/modal';
+import { ApiService } from 'src/app/services/api/api.service';
 
 @Component({
   selector: 'app-add-new-url-modal',
@@ -7,5 +8,13 @@ import { MdbModalRef } from 'mdb-angular-ui-kit/modal';
   styleUrls: ['./add-new-url-modal.component.css']
 })
 export class AddNewUrlModalComponent {
-  constructor(public modalRef: MdbModalRef<AddNewUrlModalComponent>) {}
+  newUrl:string='';
+  constructor(
+    public modalRef: MdbModalRef<AddNewUrlModalComponent>,
+    private apiService: ApiService
+    ) {}
+
+    addNewUrl():void{
+      console.log("SEND TO SERVER" + this.newUrl);
+    }
 }
